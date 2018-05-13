1155215a3ea1e555d1ae2fc920135e83ef2d83e2
Enis Mulic <enis.s.mulic@gmail.com>
2018-05-13 17:47:34 +0200

    Update Grupa A - Zadatak 3.cpp

--- Ispiti/2018-01-27/Grupa A - Zadatak 3.cpp
+++ Ispiti/2018-01-27/Grupa A - Zadatak 3.cpp
@@ -1,3 +1,13 @@
+/*
+  Napisati program koji će učitati niz od 50 integer vrijednosti. Napisati funkciju koja će
+  provjeriti da li se u nizu nalazi sekvenca od minimalno 4 uzastopna broja. Funkcija treba
+  da provjeri i prebroji koliko takvih sekvenci postoji u nizu i da ispise broj takvih sekvenci.
+  
+  Npr. 100, 322, 453, 454, 455, 456, 355, 235, 235, 236, 237, 238, 239, 111
+  453, 454, 455, 456 - 1 Sekvenca
+  235, 236, 237, 238, 239 - 2 Sekvenca
+*/
+
 #include <iostream>
  
 using namespace std;
