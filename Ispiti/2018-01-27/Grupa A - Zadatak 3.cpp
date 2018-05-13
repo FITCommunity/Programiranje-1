c720b8873b32b5e4d34c6888f5578223063a2bc6
Enis Mulic <enis.s.mulic@gmail.com>
2018-05-13 17:48:56 +0200

    Update Grupa A - Zadatak 3.cpp

--- Ispiti/2018-01-27/Grupa A - Zadatak 3.cpp
+++ Ispiti/2018-01-27/Grupa A - Zadatak 3.cpp
@@ -3,9 +3,9 @@
   provjeriti da li se u nizu nalazi sekvenca od minimalno 4 uzastopna broja. Funkcija treba
   da provjeri i prebroji koliko takvih sekvenci postoji u nizu i da ispise broj takvih sekvenci.
   
-  Npr. 100, 322, 453, 454, 455, 456, 355, 235, 235, 236, 237, 238, 239, 111
-  453, 454, 455, 456 - 1 Sekvenca
-  235, 236, 237, 238, 239 - 2 Sekvenca
+  Npr. 100, 322, 453, 454, 455, 456, 355, 235, 235, 236, 237, 238, 239, 111...
+  453, 454, 455, 456 - 1. Sekvenca
+  235, 236, 237, 238, 239 - 2. Sekvenca
 */
 
 #include <iostream>
