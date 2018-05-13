5e5ec6af6fc10ff550c9755c72bc12e6c16ef62c
Enis Mulic <enis.s.mulic@gmail.com>
2018-05-13 16:49:45 +0200

    Update Grupa B - Zadatak 4.cpp

--- Ispiti/2018-02-17/Grupa B - Zadatak 4.cpp
+++ Ispiti/2018-02-17/Grupa B - Zadatak 4.cpp
@@ -1,3 +1,32 @@
+/*
+    Dat je 2D niz koji simulira šahovsku tablu. Omogućiti korisniku unos cjelobrojnih elemenata 2D niza
+    tako da se u svako "crno" polje unese parni broj sa neparnim brojem cifara, a u "bijelo polje neparni
+    broj sa parnim brojem cifara. Izračunati i ispisati prosjeke (aritmetičke sredine) svih elemenata na 
+    bijelim poljima iznad glavne dijagonale te na crnim poljima ispod sporedne dijagonale.
+    
+    C - Crna
+    B - Bijela
+    
+    +-----+-----+-----+-----+-----+-----+-----+-----+
+    | C00 | B01 | C02 | B03 | C04 | B05 | C06 | B07 |
+    +-----+-----+-----+-----+-----+-----+-----+-----+
+    | B10 | C11 | B12 | C13 | B14 | C15 | B16 | C17 |
+    +-----+-----+-----+-----+-----+-----+-----+-----+
+    | C20 | B21 | C22 | B23 | C24 | B25 | C26 | B27 |
+    +-----+-----+-----+-----+-----+-----+-----+-----+
+    | B30 | C31 | B32 | C33 | B34 | C35 | B36 | C37 |
+    +-----+-----+-----+-----+-----+-----+-----+-----+
+    | C40 | B41 | C42 | B43 | C44 | B45 | C46 | B47 |
+    +-----+-----+-----+-----+-----+-----+-----+-----+
+    | B50 | C51 | B52 | C53 | B54 | C55 | B56 | C57 |
+    +-----+-----+-----+-----+-----+-----+-----+-----+
+    | C60 | B61 | C62 | B63 | C64 | B65 | C66 | B67 |
+    +-----+-----+-----+-----+-----+-----+-----+-----+
+    | B70 | C71 | B72 | C73 | B74 | C75 | B76 | C77 |
+    +-----+-----+-----+-----+-----+-----+-----+-----+
+    
+*/
+
 #include <iostream>
  
 using namespace std;
