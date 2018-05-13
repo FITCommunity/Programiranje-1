347785c540daa50d6f6a33bbea18fd0487ec744f
Enis Mulic <enis.s.mulic@gmail.com>
2018-05-13 17:42:24 +0200

    Update Grupa A - Zadatak 2.cpp

--- Ispiti/2018-01-27/Grupa A - Zadatak 2.cpp
+++ Ispiti/2018-01-27/Grupa A - Zadatak 2.cpp
@@ -1,3 +1,10 @@
+/*
+  Svaki paran broj se može prikazati kao suma dva prosta broja (tkz. Goldbachovo pravilo).
+  Napisati program koji će najprije učitati dva prirodna broja n1 i n2 (n2 > n1). Zatim
+  prikazati sve parne brojeve u intervalu od n1 do n2 kao sumu dva prosta broja. U glavnom
+  programu samo unijeti navedena dva prirodna broja i pozvati funkciju koja obavlja zadani posao.
+*/
+
 #include <iostream>
  
 using namespace std;
