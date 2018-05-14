8a1c7a0ae914924200e163ea26af9a7567502d1f
Enis Mulic <enis.s.mulic@gmail.com>
2018-05-14 21:50:31 +0200

    Create Zadatak 04.cpp

---
+++ Vjezbe/2017-12-28/Zadatak 04.cpp
@@ -0,0 +1,85 @@
+/*
+  Napišite program u kojem ćete kreirati matricu 5x5, te napišite i pozovite sljedeće funkcije:
+  o funkciju koja će inicijalizirati matricu da izgleda ovako:
+    0  1  1  1  1
+    1  0  1  1  1
+    1  1  0  1  1
+    1  1  1  0  1
+    1  1  1  1  0
+  o funkciju koja će izračunati sumu elemenata na sporednoj dijagonali;
+  o funkciju koja će promijeniti vrijednosti elemenata matrice tako da nakon poziva 
+    funkcije matrica izgleda ovako:
+    1  0  0  0  0
+    0  1  0  0  0
+    0  0  1  0  0
+    0  1  0  1  0
+    0  0  0  0  1
+    
+  Broj i vrstu parametara, kao i o povratne vrijednosti funkcija odredite sami.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void niz_nula_jedan(int niz[][5])
+{
+    for(int i = 0; i < 5; i++)
+    {
+        for(int j = 0; j < 5; j++)
+        {
+            if(i == j) niz[i][j] = 0;
+            else niz[i][j] = 1;
+        }
+    }
+}
+
+void niz_jedan_nula(int niz[][5])
+{
+    for(int i = 0; i < 5; i++)
+    {
+        for(int j = 0; j < 5; j++)
+        {
+            if(i == j) niz[i][j] = 1;
+            else niz[i][j] = 0;
+        }
+    }
+}
+
+int suma_dijagonale(int niz[][5])
+{
+    int suma = 0;
+    
+    for(int i = 0; i < 5; i++)
+        for(int j = 0; j < 5; j++)
+            if(i + j == 4) suma += niz[i][j] // i + j == duzina - 1
+
+    return suma;
+}
+
+void ispis(int niz[][5])
+{
+    for(int i = 0; i < 5; i++)
+    {
+        for(int j = 0; j < 5; j++)
+        {
+            cout << niz[i][j] << " "; 
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int niz[5][5];
+    
+    niz_nula_jedan(niz);
+    ispis(niz);
+    
+    cout << "Suma sporedne dijagonale: " << suma_dijagonale(niz) << endl;
+    
+    niz_jedan_nula(niz);
+    ispis(niz);
+    
+    return 0;
+}
