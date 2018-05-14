142318c52bf68927a43b83244aeb19d40eade18e
Enis Mulic <enis.s.mulic@gmail.com>
2018-05-14 21:55:57 +0200

    Create Zadataka 06.cpp

---
+++ Vjezbe/2017-12-28/Zadataka 06.cpp
@@ -0,0 +1,79 @@
+/*
+  Napišite  program  u  kojem  ćete  omogućiti  unos  šesteroznamenkastog 
+  prirodnog broja i kreiranje niza od 6 cjelobrojnih elemenata. 
+  • definirati  i  upotrijebiti  funkciju  void  inicijaliziraj(int[],  int,  int);  koja  će 
+    elemente  niza  inicijalizirate  tako  da  svakom  od  elemenata  niza  dodijeli  po  jednu 
+    znamenku unesenog šesteroznamenkastog prirodnog broja;
+  • definirati i upotrijebiti funkciju float aritmeticka(int [], int); koja će izračunati 
+    aritmetičku sredinu (prosječnu vrijednost) elemenata niza;
+  • definirati  i  upotrijebiti  funkciju  int  pronaci(int  [],  int,   float)  koja  treba 
+    pronaći  i  vratiti  element  niza  koji  je  po  svojoj  vrijednosti  najbliži  prethodno 
+    izračunatoj aritmetičkoj sredini;
+    
+  Ispis  aritmetičke  sredine  i  elementa  koji  je  po  svojoj  vrijednosti  najbliži  aritmetičkoj 
+  sredini vršiti u funkciji main.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void inicijaliziraj(int niz[], int broj, int duzina)
+{
+
+    for(int i = duzina - 1; i >= 0; i--)
+    {
+        niz[i] = broj % 10;
+        broj /= 10;
+    }
+    
+    for(int i = 0; i < duzina; i++)
+        cout << niz[i] << " ";
+}
+float aritmeticka(int niz[], int duzina)
+{
+    int suma = 0;
+    for(int i = 0; i < duzina; i++)
+        suma += niz[i];
+        
+    return suma / duzina;
+}
+
+int pronaci(int niz[], int duzina, float prosjek)
+{
+    float razlika[6];
+    int min;
+    
+    for(int i = 0; i < duzina; i++)
+    {
+        razlika[i] = (float)niz[i] - prosjek;
+        if(razlika[i] < 0) razlika[i] = -razlika[i];
+    }
+    
+    
+    
+    min = 0;
+    for(int i = 1; i < duzina; i++)
+        if(razlika[i] < razlika[min]) min = i;
+    
+    return niz[min];
+}
+
+int main()
+{
+    int niz[6];
+    int broj;
+    float prosjek;
+    
+    cout << "Unesite 6 znamenkasti broj: " << endl;
+    cin >> broj;
+    
+    inicijaliziraj(niz, broj, 6);
+    prosjek = aritmeticka(niz, 6);
+    
+    
+    cout << "Prosjek: " << prosjek << endl;
+    cout << "Najbliza cifra prosjeku je: " << pronaci(niz, 6, prosjek) << endl;
+    
+    return 0;
+}
