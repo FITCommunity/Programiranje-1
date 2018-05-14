0d40d10f9d12d77a5deb5dcc364aa5b659234ac1
Enis Mulic <enis.s.mulic@gmail.com>
2018-05-14 21:44:01 +0200

    Create Zadatak 03.cpp

---
+++ Vjezbe/2017-12-28/Zadatak 03.cpp
@@ -0,0 +1,56 @@
+/*
+  Napišite program poštujući sve faze procesa programiranja u kojem ćete omogućiti:
+  o  unos elemenata dva niza od po 9 znakova pomoću funkcije: void unos (char [], int);
+  o  uz pomoć funkcije int slova(char [], char [], int); izračunati i vratiti 
+     ukupan broj slova (bez obzira na to jesu li slova mala ili velika) u oba niza;
+  o  uz  pomoć  funkcije:  int  vokali(char  [],  int);  prebrojati  koliko  među 
+     elementima prvog niza ima samoglasnika (vokala) .
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void unos(char niz[], int duzina)
+{
+    cout << "Unesite niz: " << endl;
+    for(int i = 0; i < duzina; i++)
+        cin >> niz[i];
+    
+}
+
+int slova(char niz1[], char niz2[], int duzina)
+{
+    int brojac = 0;
+    
+    for(int i = 0; i < duzina; i++)
+    {
+        if((niz1[i] >= 'A' && niz1[i] <= 'Z') || (niz1[i] >= 'a' && niz1[i] <= 'z')) brojac++;
+        if((niz2[i] >= 'A' && niz2[i] <= 'Z') || (niz2[i] >= 'a' && niz2[i] <= 'z')) brojac++;
+    }
+    
+    return brojac;
+}
+
+int vokali(char niz[], int  duzina)
+{
+    char samoglasnici[10] = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+    int brojac = 0;
+    for(int i = 0; i < duzina; i++)
+        for(int j = 0; j < 10; j++)
+            if(niz[i] == samoglasnici[j])
+                brojac++;
+         
+    return brojac;
+}
+
+int main()
+{
+    char niz1[9], niz2[9];
+    
+    unos(niz1, 9);
+    unos(niz2, 9);
+    
+    cout << "Slova: " << slova(niz1, niz2, 9) << endl;
+    cout << "U 1. nizu ima " << vokali(niz1, 9) << " vokala" << endl;
+}
