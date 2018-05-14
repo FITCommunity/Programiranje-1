4aa25a68c8d2106be3f9fb62a85f38311f97f7eb
Enis Mulic <enis.s.mulic@gmail.com>
2018-05-14 13:20:05 +0200

    Update and rename Zadatak 5.cpp to Zadatak 05.cpp

--- Vjezbe/2017-01-17/Zadatak 05.cpp
+++ Vjezbe/2017-01-17/Zadatak 05.cpp
@@ -9,20 +9,20 @@
   D (djelimično zadovoljava) i 
   E (ne zadovoljava). 
   Napisati program koji će implementirati i testirati sljedeće funkcije:
-  •	evaluiraj – funkcija omogućava unos evaluacije za određenog (odabranog u glavnom programu) studenta 
-    (unosi se samo jedan red matrice), na način da su dozvoljene samo vrijednosti usvojene prethodnom 
-    skalom (A, B, C, D i E). U slučaju unosa bilo kojeg karaktera izvan ovog opsega, potrebno je ponoviti unos. 
-    Prilikom unosa ispisati poruku za svaku kompetenciju na sljedeći način: „Unesite evaluaciju 
-    za kompetenciju C1“, i tako redom za svaku kolonu.
-  •	najuspjesnija_kompetencija – funkcija pronalazi kompetenciju (u oba studijska programa) koja je 
-    najuspješnija, odnosno na kojoj su studenti ostvarili najbolji rezultat, te vraća i prosječnu 
-    ocjenu za tu kompetenciju. 
-    Napomena: Pridružiti svakoj oznaci numeričku vrijednost, radi jednostavnosti kalkulacije i poređenja 
-    uspjeha pojedinih kompetencija. Koristiti prosljeđivanje najmanje jednog parametra po referenci, 
-    te ispis rezultata ne vršiti u funkciji.
-  •	najuspjesniji_student – funkcija pronalazi najuspješnijeg studenta u odabranom studijskom programu 
-    (studijski program se bira u glavnom programu putem vrijednost 1 (prvih 5 redova) ili 2 (drugih 5 redova),
-    te vraća i prosječnu ocjenu tog studenta. Vrijede iste napomene kao i za prethodnu funkciju.
+  •evaluiraj – funkcija omogućava unos evaluacije za određenog (odabranog u glavnom programu) studenta 
+   (unosi se samo jedan red matrice), na način da su dozvoljene samo vrijednosti usvojene prethodnom 
+   skalom (A, B, C, D i E). U slučaju unosa bilo kojeg karaktera izvan ovog opsega, potrebno je ponoviti unos. 
+   Prilikom unosa ispisati poruku za svaku kompetenciju na sljedeći način: „Unesite evaluaciju 
+   za kompetenciju C1“, i tako redom za svaku kolonu.
+  •najuspjesnija_kompetencija – funkcija pronalazi kompetenciju (u oba studijska programa) koja je 
+   najuspješnija, odnosno na kojoj su studenti ostvarili najbolji rezultat, te vraća i prosječnu 
+   ocjenu za tu kompetenciju. 
+   Napomena: Pridružiti svakoj oznaci numeričku vrijednost, radi jednostavnosti kalkulacije i poređenja 
+   uspjeha pojedinih kompetencija. Koristiti prosljeđivanje najmanje jednog parametra po referenci, 
+   te ispis rezultata ne vršiti u funkciji.
+  •najuspjesniji_student – funkcija pronalazi najuspješnijeg studenta u odabranom studijskom programu 
+   (studijski program se bira u glavnom programu putem vrijednost 1 (prvih 5 redova) ili 2 (drugih 5 redova),
+   te vraća i prosječnu ocjenu tog studenta. Vrijede iste napomene kao i za prethodnu funkciju.
   
   U glavnom programu testirati implementirane funkcije na način da se uz uspjeh kompetencije ili studenta 
   definiše opisna evaluacija (ekspert, stručnjak,...). Dodatno ispisati podatke o najuspješnijem studentu, 
