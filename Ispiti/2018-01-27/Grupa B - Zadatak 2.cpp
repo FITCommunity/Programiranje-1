f39b581666e25e385977c426af4915dfeeb6dd49
Enis Mulic <enis.s.mulic@gmail.com>
2018-05-15 18:53:01 +0200

    Update Grupa B - Zadatak 2.cpp

--- Ispiti/2018-01-27/Grupa B - Zadatak 2.cpp
+++ Ispiti/2018-01-27/Grupa B - Zadatak 2.cpp
@@ -1,3 +1,10 @@
+/*
+  Napisati program koji će omogućiti korisniku unos dva minimalno trocifrena
+  prirodna broja m i n (n > m, m > 100, n < 500). Zatim napraviti funkciju koja
+  će vratiti aritmetičku sredinu svih srednjih cifara svih brojeva u rangu od m
+  do n. Također ta funkcija treba ispisati najveću srednju cifru navedenog ranga.
+*/
+
 #include <iostream>
 
 using namespace std;
