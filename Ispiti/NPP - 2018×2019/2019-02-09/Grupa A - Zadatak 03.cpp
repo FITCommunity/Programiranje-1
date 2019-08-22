8d3ee261660e64478fcfa0dab7786f6fdef365e0
Enis Mulić <enis.s.mulic@gmail.com>
2019-08-22 11:29:16 +0200

    Ispravka kod overflow-a za faktorijel

--- Ispiti/NPP - 2018×2019/2019-02-09/Grupa A - Zadatak 03.cpp
+++ Ispiti/NPP - 2018×2019/2019-02-09/Grupa A - Zadatak 03.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
+#include <climits>
 
 using namespace std;
 
 void Ispisi(int *niz, int velicina)
 {
-  	for(int i = 0; i < velicina; i++)
-    		cout << *(niz + i) << endl;
+	for(int i = 0; i < velicina; i++)
+    		cout << i << "! = " << *(niz + i) << endl;
 }
 
 void Popuni(int *niz, int velicina, int i = 0)
 {
-  	if(i < 0 || i >= velicina || velicina < 0)
+	if(i < 0 || i >= velicina || velicina < 0)
     		return;
-
- 	int temp = i == 0 ? 1 : *(niz + i - 1) * (i + 1);
 	
-	if(temp < 0) return;
+	// Ako je INT_MAX / prethodnim_faktorijelom manji od broja_za_koji_trazimo_faktorijel
+	// onda ce prethodni_faktorijel * broja_za_koji_trazimo_faktorijel btit vece od INT_MAX
+	// sto ce dovesti do overflow-a
+	// za int (32-bitni signed int) 12! ce biti maksimalni broj koji se moze izracunati
+	if(i > 0 && INT_MAX / *(niz + i - 1) < i)
+    		return;
+  
+	int temp = i == 0 ? 1 : *(niz + i - 1) * i;
+	
+	
   
   	*(niz + i) = temp;
   	Popuni(niz, velicina, i + 1);
