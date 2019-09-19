22c4d2d0bbd3457eab0e4bc03c25cb1b5b473072
Enis Mulić <enis.s.mulic@gmail.com>
2019-09-20 00:13:01 +0200

    Ispravka: Potencija; Autor: Selim Mekic

--- Ispiti/NPP - 2014×2015/2018-08-25/Grupa A - Zadatak 01.cpp
+++ Ispiti/NPP - 2014×2015/2018-08-25/Grupa A - Zadatak 01.cpp
@@ -1,33 +1,49 @@
 #include <iostream>
 #include <cmath>
- 
+
 using namespace std;
 
-int Potencija(int broj)
+/*
+kako se trazi da se nade broj m koji je veci ili jednak n krece se od n 
+i trazi se broj l (veci ili jednak 2 a manji od sqrt(n) + 1) koji broj n dijeli k puta (tj. dobije se n = k ^ l)
+ukoliko je broj m potencija nekog broja dijeljenjem sa brojem l k puta ce ga svesti na 1 i pronasli smo trazeni broj m,
+ukoliko broj m nije potencija treba ga inkrementirati i ponoviti proces
+*/
+
+int getPotenciju(int n)
 {
-  int granica = 10;
-  for(int k = 2; k <= granica; k++)
-    for(int l = 2; l <= granica; l++)
+  int tempBroj;
+  while(1)
+  {
+    int l = 2;
+    while(l < sqrt(n) + 1)
     {
-      int m = pow(l, k);
-      if(m >= broj)
-        return m;
+      tempBroj = n;
+      int k = 0;
+      while(tempBroj % l == 0)
+      {
+        tempBroj /= l;
+        k++;
+      }
+        
+      if(tempBroj == 1 && k >= 2) return n;
+      l++;
     }
+
+    n++;
+  }
+
+  return n;
 }
- 
-int main()
+
+int main() 
 {
     int n;
-       
-    do
-    {
-        cout << "Unesite broj: " << endl;
-        cin >> n;
-       
-    }while(n < 1);
-   
-    cout << Potencija(n);
-   
+    while(cout << "Unesite broj: ", cin >> n, n <= 0);
+
+    
+
+    cout << getPotenciju(n) << endl;
     system("pause>0");
     return 0;
 }
