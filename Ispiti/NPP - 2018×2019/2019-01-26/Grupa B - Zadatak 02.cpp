a83231d3b14e3b11466ee5425c85f6f40cc0e1a1
PancakeAlchemist <enis.s.mulic@gmail.com>
2020-01-25 19:03:33 +0100

    Palindrom pojednostavljen

--- Ispiti/NPP - 2018×2019/2019-01-26/Grupa B - Zadatak 02.cpp
+++ Ispiti/NPP - 2018×2019/2019-01-26/Grupa B - Zadatak 02.cpp
@@ -3,36 +3,36 @@
 
 using namespace std;
 
-int duzina(int broj)
+int obrni(int broj)
 {
-	int brojac = 0;
-	while (broj)
-	{
-		brojac++;
-		broj /= 10;
-	}
-	return brojac;
+    int noviBroj = 0;
+    while(broj)
+    {
+        noviBroj = noviBroj * 10 + broj % 10;
+        broj /= 10;
+    }
+
+    return noviBroj;
 }
-bool isProstPalindrom(int broj)
+
+bool isPalindrom(int broj)
 {
-	for (int i = 2; i * i < broj; i++)
+    return broj == obrni(broj);
+}
+
+bool isProst(int broj)
+{
+    for (int i = 2; i * i <= broj; i++)
 		if (broj % i == 0)
 			return false;
-
-	int d1 = 0;
-	int d2 = duzina(broj);
-	while (d1 != d2 / 2)
-	{
-		int c1 = int(broj / pow(10, d1)) % 10;
-		int c2 = int(broj / pow(10, d2 - 1)) % 10;
-
-		if (c1 != c2) 
-			return false;
-		d1++;
-		d2--;
-	}
-	return true;
+    return true;
 }
+
+bool isProstPalindrom(int broj)
+{
+    return isProst(broj) && isPalindrom(broj);
+}
+
 int main()
 {
 	
@@ -41,6 +41,7 @@ int main()
 		if (isProstPalindrom(broj))
 			cout << "Broj " << broj << " je i prost i palindrom" << endl;
 
+  
 	system("pause>0");
 	return 0;
 }
